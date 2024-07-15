7c7642ed23f36926b290ed6b1b3a4deb66044525
Mownitaa <mabiamownita@gmail.com>
2024-07-15 13:06:28 +0600

    implemented search by city

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const apiKey = "a0e794768c2fa80a066cea31ae8d0423";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
+const input = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
 async function checkWeather(city){
-const response = await fetch (apiUrl + city + `&appId = ${apiKey}`);
+const response = await fetch (apiUrl + city + `&appid=${apiKey}`);
 var data = await response.json();
 
-console.log(data)
+console.log(data);
+
+document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 }
+
+searchBtn.addEventListener("click",()=>{
+    checkWeather(input.value);
+})
