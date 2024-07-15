290f0a93019c54c6990830e0817035dbcbf4d60d
Mownitaa <mabiamownita@gmail.com>
2024-07-15 13:14:51 +0600

    implemented conditional weather image

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const input = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city){
 const response = await fetch (apiUrl + city + `&appid=${apiKey}`);
@@ -10,10 +11,24 @@ var data = await response.json();
 
 console.log(data);
 
-document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "images/cloudy-weather.jpg"
+    }
+    else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "images/rainy-weather.png"
+    }
+    else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "images/clear-weather.avif"
+    }
+    else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "images/clear-weather.png"
+    }
 }
 
 searchBtn.addEventListener("click",()=>{
